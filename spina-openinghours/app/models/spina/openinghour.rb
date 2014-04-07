9b9d88ecfbebe5db1aeebde00aff613a6ab0ba38
Bram Jetten <mail@bramjetten.nl>
2014-04-07 17:27:14 +0200

    Openinghour

--- spina-openinghours/app/models/spina/openinghour.rb
+++ spina-openinghours/app/models/spina/openinghour.rb
@@ -1,12 +1,20 @@
 module Spina
   class Openinghour < ActiveRecord::Base
-    validates :day, presence: true, uniqueness: true
+    validate :day_must_be_present_and_unique
 
     def openingtimes
       if start_time.present? && end_time.present?
         [start_time, end_time].join(' - ')
       else
         "Gesloten"
+      end
+    end
+
+    private
+
+    def day_must_be_present_and_unique
+      if day.blank? || Openinghour.where(day: day).exists?
+        errors.add(:day, 'moet uniek zijn')
       end
     end
   end
