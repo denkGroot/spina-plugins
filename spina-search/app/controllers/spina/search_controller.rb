ea3a1d5f0b7da2b334ece8e60d8ba68c42e60bbc
Bram Jetten <mail@bramjetten.nl>
2015-04-30 11:32:11 +0200

    Search all pages

--- spina-search/app/controllers/spina/search_controller.rb
+++ spina-search/app/controllers/spina/search_controller.rb
@@ -2,7 +2,7 @@ module Spina
   class SearchController < ApplicationController
     def query
       query = params[:q]
-      render json: Page.active.where(view_template: 'documentation').where('title LIKE ?', "%#{query}%").to_json
+      render json: Page.active.where('title LIKE ?', "%#{query}%").to_json
     end
   end
 end
